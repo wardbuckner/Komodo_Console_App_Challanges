d7cb75ecbb3571b76b78581fdfd45bd65503f9df
Ward Buckner <wbuckner14@gmail.com>
2020-10-01 11:51:46 -0400

    Add project files.

---
+++ WWB_Komodo_Cafe_Class_Library/Cafe_Console.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WWB_Komodo_Cafe_Class_Library
+{
+    public class Cafe_Console
+    {
+    }
+}
---
+++ WWB_Komodo_Cafe_Console_App/Cafe_Class_1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WWB_Komodo_Cafe_Console_App
+{
+    class Cafe_Class_1
+    {
+    }
+}
